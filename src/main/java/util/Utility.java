1164833f662aa781f69b2e9d901e9ba824d1aece
Daniel <donnadielito@gmail.com>
2024-05-31 08:52:01 -0600

    utility arreglao

--- src/main/java/util/Utility.java
+++ src/main/java/util/Utility.java
@@ -1,5 +1,7 @@
 package util;
 
+import domain.BTree;
+
 import java.util.Random;
 
 public class Utility {
@@ -41,5 +43,31 @@ public class Utility {
         Comparable comp2 = (Comparable) element2;
 
         return comp1.compareTo(comp2) > 0;
+    }
+
+    public static int compare(Object a, Object b) {
+        switch (instanceOf(a, b)){
+            case "Integer":
+                Integer int1 = (Integer)a; Integer int2 = (Integer)b;
+                return int1 < int2 ? -1 : int1 > int2 ? 1 : 0; //0 == equal
+            case "String":
+                String st1 = (String)a; String st2 = (String)b;
+                return st1.compareTo(st2)<0 ? -1 : st1.compareTo(st2) > 0 ? 1 : 0;
+            case "Character":
+                Character c1 = (Character)a; Character c2 = (Character)b;
+                return c1.compareTo(c2)<0 ? -1 : c1.compareTo(c2)>0 ? 1 : 0;
+            case "Btree":
+                BTree b1 = (BTree) a; BTree b2 = (BTree) b;
+                return b1.toString().compareTo(b2.toString())<0? -1 : b1.toString().compareTo(b2.toString())>0 ? 1 : 0;
+        }
+        return 2; //Unknown
+    }
+
+    private static String instanceOf(Object a, Object b) {
+        if(a instanceof Integer && b instanceof Integer) return "Integer";
+        if(a instanceof String && b instanceof String) return "String";
+        if(a instanceof Character && b instanceof Character) return "Character";
+        if(a instanceof BTree && b instanceof BTree) return "Btree";
+        return "Unknown";
     }
 }
